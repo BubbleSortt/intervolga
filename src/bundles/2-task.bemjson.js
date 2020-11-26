62b590ed853a29c521c8309f44955037bd52ad4f
bubblesort <aleksanrmerklin@gmail.com>
2020-11-26 15:04:51 +0400

    A little edits

--- src/bundles/2-task.bemjson.js
+++ src/bundles/2-task.bemjson.js
@@ -14,14 +14,14 @@ module.exports = {
                   block: 'login', mix: {block: 'form', elem: 'input-wrap'},
                   content: [
                     {elem: 'label', tag: 'label', attrs: {for: 'login'}, content: 'Введите логин:'},
-                    {elem: 'input', tag: 'input', attrs: {id: 'login', name: 'login', type: 'text', placeholder: 'Артём Звездилин'}},
+                    {elem: 'input', tag: 'input', cls: 'input', attrs: {id: 'login', name: 'login', type: 'text', placeholder: 'Артём Звездилин'}},
                   ],
                 },
                 {
                   block: 'password', mix: {block: 'form', elem: 'input-wrap'},
                   content: [
                     {elem: 'label', tag: 'label', attrs: {for: 'password'}, content: 'Введите пароль:'},
-                    {elem: 'input', tag: 'input', attrs: {id: 'password', name: 'password', type: 'password'}},
+                    {elem: 'input', tag: 'input', cls: 'input', attrs: {id: 'password', name: 'password', type: 'password'}},
                   ],
                 },
               ],
