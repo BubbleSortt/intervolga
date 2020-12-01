4051a9ac3b90acb54d9320a872276e15f5120b38
bubblesort <aleksanrmerklin@gmail.com>
2020-12-01 12:58:59 +0400

    The task completed

---
+++ src/bundles/11-task.bemjson.js
@@ -0,0 +1,12 @@
+module.exports = {
+  block: 'page',
+  title: 'Ярлык к плитке',
+  content: [
+    {
+      cls: 'container',
+      content: [
+        {block: 'card'},
+      ],
+    },
+  ],
+};
